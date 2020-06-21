d62b950d4dd66ce4f7cd1d5f1d6cf40a60e4b09b
Zhanna Pasich <pasich.zh@gmail.com>
2020-06-21 18:24:42 +0200

    added Exceptions (FileNotFoundError and PageNotFoundError); added separate respond methods; fixed file opening (via Path class and with-block);

--- src/server.py
+++ src/server.py
@@ -2,18 +2,23 @@ import socketserver
 import os
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
-import codecs
+from typing import Union
 from datetime import datetime
+from pathlib import Path
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT={PORT}")
+
+
+class PageNotFoundError(Exception):
+    pass
 
 
 def get_page(self):
     path, qs = self.path.split("?") if '?' in self.path else [self.path, ""]
 
     if path.endswith(".css"):
-        return get_cv_style()
+        get_cv_style(self)
 
     path = path.rstrip('/')
     switcher = {
@@ -35,37 +40,48 @@ def get_page(self):
     # handler = switcher.get(path, default_handler)
     # handler()
 
-    return switcher[path](qs) if path in switcher else SimpleHTTPRequestHandler.do_GET(self)
+    try:
+        if path in switcher:
+            switcher[path](self, qs)
+        else:
+            # return SimpleHTTPRequestHandler.do_GET(self)
+            raise PageNotFoundError
+    except FileNotFoundError:
+        respond_404(self)
+    except PageNotFoundError:
+        respond_405(self)
 
 
-def get_page_hello(qs):
+def get_page_hello(self, qs) -> None:
     if qs != "":
         qs = parse_qs(qs)  # return dict
     name = get_name(qs)
     year = get_year(qs)
-    return f"""
-                    <p>Hey {name}!</p>
-                    <p>You were born in {year}.</p>
+    msg = f"""
+                    Hey {name}!
+                    You were born in {str(year)}.
                 """
+    respond_200(self, msg, "text/plain")
 
 
-def get_page_goodbye(qs):
-    return f"""
-                    <p>{say_bye(datetime.today().hour)}</p>
-                    <p>Time: {datetime.today()}</p>
+def get_page_goodbye(self, qs) -> None:
+    msg = f"""
+                    {say_bye(datetime.today().hour)}
+                    Time: {datetime.today()}
                  """
+    respond_200(self, msg, "text/plain")
 
 
-def get_name(qs):
+def get_name(qs) -> str:
         return qs["name"][0] if "name" in qs else "Dude"
 
 
-def get_year(qs):
+def get_year(qs) -> Union[int, str]:
     today = datetime.today().year
-    return str(today - int(qs["age"][0])) if "age" in qs else "-"
+    return today - int(qs["age"][0]) if "age" in qs else "-"
 
 
-def say_bye(hour):
+def say_bye(hour) -> str:
     if hour < 0:
         return "Invalid value."
     elif hour < 6 or hour == 23:
@@ -80,42 +96,79 @@ def say_bye(hour):
         return "Invalid value."
 
 
-def get_cv_style():
-    return codecs.open("pages/cv_style.css", "r", "utf-8").read()
+def get_cv_style(self) -> None:
+    msg = get_file_contents("pages/cv_style.css")
+    respond_200(self, msg, "text/css")
 
 
-def get_page_cv(qs):
-    return codecs.open("pages/cv_common.html", "r", "utf-8").read()
+def get_page_cv(self, _qs) -> None:
+    msg = get_file_contents("pages/cv_common.html")
+    respond_200(self, msg, "text/html")
 
 
-def get_page_cv_education(qs):
-    return codecs.open("pages/cv_education.html", "r", 'utf-8').read()
+def get_page_cv_education(self, _qs) -> None:
+    msg = get_file_contents("pages/cv_education.html")
+    respond_200(self, msg, "text/html")
 
 
-def get_page_cv_job(qs):
-    return codecs.open("pages/cv_job.html", "r", 'utf-8').read()
+def get_page_cv_job(self, _qs) -> None:
+    msg = get_file_contents("pages/cv_job.html")
+    respond_200(self, msg, "text/html")
 
 
-def get_page_cv_skills(qs):
-    return codecs.open("pages/cv_skills.html", "r", 'utf-8').read()
+def get_page_cv_skills(self, _qs) -> None:
+    msg = get_file_contents("pages/cv_skills.html")
+    respond_200(self, msg, "text/html")
+
+
+def get_file_contents(file_path) -> str:
+    full_file_path = Path(__file__).parent.parent.resolve() / file_path  # full path to dir where file is located
+    if not full_file_path.is_file():
+        raise FileNotFoundError
+    with full_file_path.open("r", encoding="utf-8") as file_src:
+        content = file_src.read()
+    return content
+
+
+def respond_200(self, msg: str, content_type="text/plain") -> None:
+    msg = msg.encode()
+
+    self.send_response(200)
+    self.send_header("Content-type", content_type)
+    self.send_header("Content-length", len(msg))
+    self.end_headers()
+
+    self.wfile.write(msg)
+
+
+def respond_404(self) -> None:
+    msg = "Error 404: File not found"
+    msg = msg.encode()
+
+    self.send_response(404)
+    self.send_header("Content-type", "text/plain")
+    self.send_header("Content-length", len(msg))
+    self.end_headers()
+
+    self.wfile.write(msg)
+
+
+def respond_405(self) -> None:
+    msg = "Error 405: Page not found"
+    msg = msg.encode()
+
+    self.send_response(405)
+    self.send_header("Content-type", "text/plain")
+    self.send_header("Content-length", len(msg))
+    self.end_headers()
+
+    self.wfile.write(msg)
 
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path != "/":
-            msg = get_page(self)
-            msg = msg.encode()
-
-            self.send_response(200)
-            if self.path.endswith(".css"):
-                self.send_header("Content-type", "text/css")
-            else:
-                self.send_header("Content-type", "text/html")
-            self.send_header("Content-length", len(msg))
-            self.end_headers()
-
-            self.wfile.write(msg)
-
+            get_page(self)
         else:
             return SimpleHTTPRequestHandler.do_GET(self)
 
