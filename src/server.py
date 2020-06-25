8f2e3007f72626614b357731fb35ccb5e68e95ed
Zhanna Pasich <pasich.zh@gmail.com>
2020-06-25 06:14:36 +0200

    add do_POST; add functions to read\write .json file;

--- src/server.py
+++ src/server.py
@@ -1,8 +1,10 @@
 import socketserver
 import os
+import json
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 from typing import Union
+from typing import Dict
 from datetime import datetime
 from pathlib import Path
 
@@ -10,15 +12,27 @@ PORT = int(os.getenv("PORT", 8000))
 print(f"PORT={PORT}")
 
 
-class PageNotFoundError(Exception):
+class PageNotFoundError(Exception): #what's Exception and RuntimeError ???
     pass
 
 
-def get_page(self):
+class MethodNotAllowed(Exception):
+    pass
+
+
+class UnknownPath(Exception):
+    pass
+
+
+def do(self, method: str) -> None:
     path, qs = self.path.split("?") if '?' in self.path else [self.path, ""]
 
+    if path == "/":
+        return SimpleHTTPRequestHandler.do_GET(self)
+
     if path.endswith(".css"):
-        get_cv_style(self)
+        get_cv_style(self, method)
+        return
 
     path = path.rstrip('/')
     switcher = {
@@ -28,6 +42,7 @@ def get_page(self):
         "/cv/job": get_page_cv_job,
         "/cv/education": get_page_cv_education,
         "/cv/skills": get_page_cv_skills,
+        "/statistics": get_page_statistics,
     }
 
     # get a page via dict.get (usable in do_GET)
@@ -39,32 +54,103 @@ def get_page(self):
     # default_handler = super().do_GET
     # handler = switcher.get(path, default_handler)
     # handler()
-
     try:
         if path in switcher:
-            switcher[path](self, qs)
+            switcher[path](self, method, qs)
         else:
             # return SimpleHTTPRequestHandler.do_GET(self)
+            print(path)
             raise PageNotFoundError
-    except FileNotFoundError:
+    except (FileNotFoundError, PageNotFoundError):
         respond_404(self)
-    except PageNotFoundError:
+    except MethodNotAllowed:
         respond_405(self)
 
 
-def get_page_hello(self, qs) -> None:
-    if qs != "":
-        qs = parse_qs(qs)  # return dict
-    name = get_name(qs)
-    year = get_year(qs)
-    msg = f"""
-                    Hey {name}!
-                    You were born in {str(year)}.
-                """
-    respond_200(self, msg, "text/plain")
+def get_page_hello(self, method: str, qs: str) -> None:
+    switcher = {
+        "GET": show_page_hello,
+        "POST": save_user_data,
+    }
+    if method in switcher:
+        switcher[method](self)
+    else:
+        raise MethodNotAllowed
+
+    # if qs != "":
+    #     qs = parse_qs(qs)  # return dict
+    # #name = get_name(qs)
+    # #year = get_year(qs)
+    # msg = get_file_contents("pages/hello.html").format(name='DJANGO', year=76) #format ???
+    # respond_200(self, msg, "text/html")
 
 
-def get_page_goodbye(self, qs) -> None:
+def show_page_hello(self):
+    #increment_page_visit(self, "/hello")
+    user_data = read_data(self, "sessions.json")
+    name = user_data["name"] if "name" in user_data else "Dude"
+    today = datetime.today().year
+    year = today - int(user_data["age"]) if "age" in user_data else "-"
+    msg = get_file_contents("pages/hello.html").format(name=name, year=year) #format ???
+    respond_200(self, msg, "text/html")
+
+
+def save_user_data(self):
+    user_data = get_user_data(self)
+    update_user_sessions(self, user_data)
+    respond_302(self, "/hello")
+
+
+# def increment_page_visit(self, endpoint: str) -> None:
+#     statistics_content = read_data(self, "visit_counters.json")
+#     today = datetime.today().date()
+#
+#     if endpoint in statistics_content:
+#         if today in statistics_content[endpoint]:
+#             print(statistics_content[endpoint][today])
+#             #statistics_content[endpoint][today] += 1
+#         else:
+#             statistics_content[endpoint][today] =
+#     print(statistics_content[endpoint][today])
+
+
+def get_user_data(self) -> Dict[str, str]:
+    content_length = int(self.headers["Content-Length"])
+    data = self.rfile.read(content_length)
+    payload = data.decode()
+    qs = parse_qs(payload)
+    user_data = {}
+    for key, values in qs.items():
+        if not values:
+            continue
+        user_data[key] = values[0]
+
+    return user_data
+
+
+def update_user_sessions(self, user_data):
+    sessions = read_data(self, "sessions.json")
+    sessions.update(user_data)  # what does update ???
+    write_data(self, "sessions.json", sessions)
+
+
+def read_data(self, path: str) -> Dict:
+    file_path = Path(__file__).parent.parent.resolve() / path
+    try:
+        with file_path.open("r", encoding="utf-8") as usf:
+            return json.load(usf)   #what does load?
+    except (json.JSONDecodeError, FileNotFoundError):
+        print("-> load_user_sessions: exception")
+        return {}   #return error!!!
+
+
+def write_data(self, path: str, sessions: Dict) -> None:
+    file_path = Path(__file__).parent.parent.resolve() / path
+    with file_path.open("w") as usf:
+        json.dump(sessions, usf)
+
+
+def get_page_goodbye(self, _method, _qs) -> None:
     msg = f"""
                     {say_bye(datetime.today().hour)}
                     Time: {datetime.today()}
@@ -96,28 +182,38 @@ def say_bye(hour) -> str:
         return "Invalid value."
 
 
-def get_cv_style(self) -> None:
+def get_cv_style(self, _method) -> None:
     msg = get_file_contents("pages/cv_style.css")
     respond_200(self, msg, "text/css")
 
 
-def get_page_cv(self, _qs) -> None:
-    msg = get_file_contents("pages/cv_common.html")
+def get_page_cv(self, _method, _qs) -> None:
+    resume_content = read_data(self, "contents/cv_resume.json")
+    msg = get_file_contents("pages/cv_common.html").format(**resume_content)
     respond_200(self, msg, "text/html")
 
 
-def get_page_cv_education(self, _qs) -> None:
-    msg = get_file_contents("pages/cv_education.html")
+def get_page_cv_education(self, _method, _qs) -> None:
+    resume_content = read_data(self, "contents/cv_resume.json")
+    msg = get_file_contents("pages/cv_education.html").format(**resume_content)
     respond_200(self, msg, "text/html")
 
 
-def get_page_cv_job(self, _qs) -> None:
-    msg = get_file_contents("pages/cv_job.html")
+def get_page_cv_job(self, _method, _qs) -> None:
+    resume_content = read_data(self, "contents/cv_resume.json")
+    msg = get_file_contents("pages/cv_job.html").format(**resume_content)
     respond_200(self, msg, "text/html")
 
 
-def get_page_cv_skills(self, _qs) -> None:
-    msg = get_file_contents("pages/cv_skills.html")
+def get_page_cv_skills(self, _method, _qs) -> None:
+    resume_content = read_data(self, "contents/cv_resume.json")
+    msg = get_file_contents("pages/cv_skills.html").format(**resume_content)
+    respond_200(self, msg, "text/html")
+
+
+def get_page_statistics(self, _method, _qs) -> None:
+    statistics_content = read_data(self, "visit_counters.json")
+    msg = get_file_contents("pages/statistics.html")
     respond_200(self, msg, "text/html")
 
 
@@ -131,34 +227,31 @@ def get_file_contents(file_path) -> str:
 
 
 def respond_200(self, msg: str, content_type="text/plain") -> None:
-    msg = msg.encode()
-
-    self.send_response(200)
-    self.send_header("Content-type", content_type)
-    self.send_header("Content-length", len(msg))
-    self.end_headers()
-
-    self.wfile.write(msg)
+    send_response(self, 200, msg, content_type)
 
 
 def respond_404(self) -> None:
     msg = "Error 404: File not found"
-    msg = msg.encode()
-
-    self.send_response(404)
-    self.send_header("Content-type", "text/plain")
-    self.send_header("Content-length", len(msg))
-    self.end_headers()
-
-    self.wfile.write(msg)
+    send_response(self, 404, msg, "text/plain")
 
 
 def respond_405(self) -> None:
-    msg = "Error 405: Page not found"
+    msg = "Error 405: Method not allowed"
+    send_response(self, 405, msg, "text/plain")
+
+
+def respond_302(self, redirect_to: str) -> None:
+    send_response(self, 302, "", "text/plain", redirect_to)
+
+
+def send_response(self, code: int, msg: str, content_type: str, redirect_to="") -> None:
     msg = msg.encode()
 
-    self.send_response(405)
-    self.send_header("Content-type", "text/plain")
+    self.send_response(code)
+    self.send_header("Content-type", content_type)
+    if redirect_to != "":
+        self.send_header("Location", redirect_to)
+    self.send_header("Content-length", len(msg))
     self.send_header("Content-length", len(msg))
     self.end_headers()
 
@@ -167,10 +260,20 @@ def respond_405(self) -> None:
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path != "/":
-            get_page(self)
-        else:
-            return SimpleHTTPRequestHandler.do_GET(self)
+        # if self.path != "/":
+        #     get_page(self)
+        # else:
+        #     return SimpleHTTPRequestHandler.do_GET(self)
+        try:
+            do(self, "GET")
+        except UnknownPath:
+            SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        try:
+            do(self, "POST")
+        except UnknownPath:
+            SimpleHTTPRequestHandler.do_GET(self)
 
 
 if __name__ == '__main__':
