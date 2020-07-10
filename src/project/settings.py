14574a5e07a5a94586f66237b4be6fb223356f71
jajabin <jkvazi@gmail.com>
2020-07-10 11:09:18 +0200

    fix django for heroku

--- src/project/settings.py
+++ src/project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from src.common import instances
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -116,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(instances.PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
